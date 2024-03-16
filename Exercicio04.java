eedd8ae869016245452d2efe801a3511025ddc28
Gabriela Rodrigues Schorr <gabibusuda123@gmail.com>
2024-03-16 19:34:23 -0300

    Exercicio04

--- Exercicio04.java
+++ Exercicio04.java
@@ -3,7 +3,15 @@ public class Exercicio04 {
 
         System.out.println("Exercicio04: ");
 
-        //
+        //4. Elaborar um programa que efetue a apresentação do valor da conversão em real (R$) de umvalor 
+        //lido em dólar (US$). O programa deverá solicitar o 
+        //valor da cotação do dólar e também aquantidade de dólares disponíveis com o usuário
+
+        double cotacaoD = Prompt.lerDecimal("Digite a cotação do dólar R$/US$: ");
+
+        double quantidadeD = Prompt.lerDecimal("Digite a quantidade de dólares disponíveis: ");
+
+        System.out.println("Valor em reais da quantidade de dólares disponíveis: R$ " + cotacaoD * quantidadeD);
 
     }
 }
