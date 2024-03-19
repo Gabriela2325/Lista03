6bd250da8c557eb7a6844951c9cff4cb05750030
Gabriela Rodrigues Schorr <gabibusuda123@gmail.com>
2024-03-19 20:23:05 -0300

    Class08

--- Exercicio08.java
+++ Exercicio08.java
@@ -8,24 +8,14 @@ public class Exercicio08 {
         //(media >= 7),Reprovado (media <= 5) e Recuperação (media entre 5.1 a 6.9)
 
         String nome = Prompt.lerLinha("Digite o nome do aluno: ");
-
         double nota1 = Prompt.lerDecimal("Digite a primeira nota do aluno: ");
         double nota2 = Prompt.lerDecimal("Digite a segunda nota do aluno: ");
         double nota3 = Prompt.lerDecimal("Digite a terceira nota do aluno: ");
 
-        double media = (nota1 + nota2 + nota3) / 3;
+        Aluno08 aluno = new Aluno08(nome, nota1, nota2, nota3);
 
-        String mencao;
-        if (media >= 7) {
-            mencao = "Aprovado";
-        } else if (media <= 5) {
-            mencao = "Reprovado";
-        } else {
-            mencao = "Recuperação";
-        }
-
-        System.out.println("Nome do aluno: " + nome);
-        System.out.println("Menção: " + mencao);
+        System.out.println(aluno);
 
     }
 }
+
