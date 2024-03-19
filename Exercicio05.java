177452a10ce98fd5fd17bcfda1271220c4eb86b1
Gabriela Rodrigues Schorr <gabibusuda123@gmail.com>
2024-03-19 20:09:13 -0300

    Class05

--- Exercicio05.java
+++ Exercicio05.java
@@ -6,9 +6,15 @@ public class Exercicio05 {
         //5. A Loja Mamão com Açúcar está vendendo seus produtos em 5 (cinco) prestações sem juros.
         //Faça um programa que receba um valor de uma compra e mostre o valor das prestações.
 
-        double compra = Prompt.lerDecimal("Digite o valor da compra: ");
+        Compra05 compra = new Compra05();
 
-        System.out.println("O valor de cada prestação é de: R$ " + compra / 5);
+        compra.setNomeProduto(Prompt.lerLinha("Digite o nome do produto: "));
+        compra.setPrecoProduto(Prompt.lerDecimal("Digite o preço do produto: "));
+        compra.setQuantidade(Prompt.lerInteiro("Digite a quantidade: "));
+
+        double valorTC = compra.calcularVT();
+
+        System.out.println("O valor de cada prestação é de: R$ " + valorTC / 5);
 
     }
 }
