65e6aa7b2f6315dba9c7724b66db7c9c4ccb208a
Gabriela Rodrigues Schorr <gabibusuda123@gmail.com>
2024-03-19 19:33:00 -0300

    Class02

--- Exercicio02.java
+++ Exercicio02.java
@@ -5,13 +5,12 @@ public class Exercicio02 {
         //2. Escrever um programa para determinar o consumo médio de um automóvel sendo fornecida
         // adistância total percorrida pelo automóvel e o total de combustível gasto.
 
-        double dTotal = Prompt.lerDecimal("Digite a distância total percorrida em KM: ");
+        double distancia = Prompt.lerDecimal("Digite a distância total percorrida (em km): ");
+        double combustivel = Prompt.lerDecimal("Digite o total de combustível gasto (em litros): ");
 
-        double Gasto = Prompt.lerDecimal("Digite o total de combustível gasto em L: ");
- 
-        double consumoM = dTotal / Gasto;
+        Automovel02 automovel = new Automovel02(distancia, combustivel);
 
-        System.out.println("O consumo médio do automóvel é de: " + consumoM + " km/l");
+        System.out.println("O consumo médio do automóvel é de: " + automovel.ConsumoMedio() + " km/l");
     }
     
 }
