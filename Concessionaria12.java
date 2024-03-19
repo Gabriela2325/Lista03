3e7fa8eb9aded75802f9c400cf57880553305a2f
Gabriela Rodrigues Schorr <gabibusuda123@gmail.com>
2024-03-19 20:43:25 -0300

    Class12

---
+++ Concessionaria12.java
@@ -0,0 +1,63 @@
+public class Concessionaria12 {
+   
+    private int totalCarrosAte2000;
+    private int totalGeral;
+    
+    public Concessionaria12(int nParam) {
+        totalCarrosAte2000 = 0;
+        totalGeral = 0;
+    }
+    
+    public Concessionaria12() {
+        totalCarrosAte2000 = 0;
+        totalGeral = 0;
+     }
+    
+    public int getTotalCarrosAte2000() {
+        return totalCarrosAte2000;
+    }
+    
+    public void setTotalCarrosAte2000(int nParam) {
+        totalCarrosAte2000 = nParam;
+    }
+    
+    public int getTotalGeral() {
+        return totalGeral;
+    }
+    
+    public void setTotalGeral(int nParam) {
+        totalGeral = nParam;
+    }
+    
+    public String toString() {
+        return "Total de carros até 2000: " + totalCarrosAte2000 + "\n" +
+            "Total geral de carros calculados: " + totalGeral;
+    }
+    
+    public void calcularDesconto() {
+        while (true) {
+            int ano = Prompt.lerInteiro("Digite o ano do veículo: ");
+            double valor = Prompt.lerDecimal("Digite o valor do veículo: ");
+    
+            double desconto;
+            if (ano <= 2000) {
+                desconto = valor * 0.12;
+                totalCarrosAte2000++;
+            } else {
+                desconto = valor * 0.07; 
+            }
+    
+    double valorAPagar = valor - desconto;
+    System.out.println("Desconto: R$" + desconto);
+    System.out.println("Valor a pagar pelo cliente: R$" + valorAPagar);
+    
+    totalGeral++;
+    
+    String continuar = Prompt.lerLinha("Deseja continuar calculando desconto? S/N: ");
+        if (!continuar.equalsIgnoreCase("S")) {
+            break;
+        }
+    }
+}
+}
+
--- Exercicio12.java
+++ Exercicio12.java
@@ -9,34 +9,9 @@ public class Exercicio12 {
         // O sistema deverá perguntar se deseja continuar calculando descontoaté que a resposta seja: 
         //“(N) Não”. Informar total de carros com ano até 2000 e total geral.
 
-        int total = 0;
-        int Geral = 0;
+        Concessionaria12 concessionaria = new Concessionaria12();
+        concessionaria.calcularDesconto();
 
-        while (true) {
-            int ano = Prompt.lerInteiro("Digite o ano do veículo: ");
-            double valor = Prompt.lerDecimal("Digite o valor do veículo: ");
-
-            double desconto;
-                if (ano <= 2000) {
-                    desconto = valor * 0.12;
-                    total++;
-                } else {
-                    desconto = valor * 0.07; 
-                }
-
-            double valorAPagar = valor - desconto;
-            System.out.println("Desconto: R$" + desconto);
-            System.out.println("Valor a pagar pelo cliente: R$" + valorAPagar);
-
-            Geral++;
-
-            String continuar = Prompt.lerLinha("Deseja continuar calculando desconto? S/N: ");
-                if (!continuar.equalsIgnoreCase("S")) {
-                    break;
-                }
-        }
-
-        System.out.println("Total de carros com ano até 2000: " + total);
-        System.out.println("Total geral de carros calculados: " + Geral);
+        System.out.println(concessionaria);
     }
 }   
