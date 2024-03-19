0896cc355a1f43649c62b2a5f1b0ebdb945f8ee3
Gabriela Rodrigues Schorr <gabibusuda123@gmail.com>
2024-03-19 20:28:40 -0300

    Class09

--- Exercicio09.java
+++ Exercicio09.java
@@ -6,17 +6,10 @@ public class Exercicio09 {
         //9. Ler 80 números e ao final informar quantos número(s) est(á)ão no intervalo entre
         // 10 (inclusive)e 150 (inclusive)
 
-        int contador = 0; 
+        Numeros09 numeros = new Numeros09();
+        numeros.lerNumeros();
 
-        for (int i = 0; i < 80; i++) {
-            int numero = Prompt.lerInteiro("Digite o número " + (i + 1) + ": ");
-
-            if (numero >= 10 && numero <= 150) {
-                contador++;
-            }
-        }
-
-        System.out.println("Quantidade de números no intervalo entre 10 e 150 (inclusive): " + contador);
+        System.out.println(numeros);
         
     }
 }
