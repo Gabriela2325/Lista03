1c9e7a29f25dff8044c60cdc678da1992b1904d3
Gabriela Rodrigues Schorr <gabibusuda123@gmail.com>
2024-03-19 20:38:05 -0300

    Class11

--- Exercicio11.java
+++ Exercicio11.java
@@ -7,28 +7,9 @@ public class Exercicio11 {
         //nome e se ela é homem ou mulher.
         //No final informe total de homens e de mulheres
 
-        int Homens = 0;
-        int Mulheres = 0;
+        PessoaSexo11 pessoas = new PessoaSexo11();
+        pessoas.lerPessoas();
 
-        for (int i = 0; i < 56; i++) {
-            String nome = Prompt.lerLinha("Digite o nome da pessoa " + (i + 1) + ": ");
-            String sexo;
-
-            do {
-                sexo = Prompt.lerLinha("Digite o sexo da pessoa (H para homem, M para mulher): ");
-                sexo = sexo.toUpperCase(); 
-            } while (!sexo.equals("H") && !sexo.equals("M")); 
-
-            if (sexo.equals("H")) {
-                System.out.println(nome + " - Homem");
-                Homens++;
-            } else if (sexo.equals("M")) {
-                System.out.println(nome + " - Mulher");
-                Mulheres++;
-            }
-        }
-
-        System.out.println("Total de homens: " + Homens);
-        System.out.println("Total de mulheres: " + Mulheres);
+        System.out.println(pessoas);
     }   
 }
---
+++ PessoaSexo11.java
@@ -0,0 +1,57 @@
+public class PessoaSexo11 {
+    private int totalHomens;
+    private int totalMulheres;
+
+    public PessoaSexo11(int nParam) {
+        totalHomens = 0;
+        totalMulheres = 0;
+    }
+
+    public PessoaSexo11() {
+        totalHomens = 0;
+        totalMulheres = 0;
+    }
+
+    public int getTotalHomens() {
+        return totalHomens;
+    }
+
+    public void setTotalHomens(int nParam) {
+        totalHomens = nParam;
+    }
+
+    public int getTotalMulheres() {
+        return totalMulheres;
+    }
+
+    public void setTotalMulheres(int nParam) {
+        totalMulheres = nParam;
+    }
+
+    public String toString() {
+        return "Total de homens: " + totalHomens + "\n" +
+               "Total de mulheres: " + totalMulheres;
+    }
+
+    public void lerPessoas() {
+        System.out.println("Digite o nome e o sexo de cada pessoa:");
+
+        for (int i = 0; i < 56; i++) {
+            String nome = Prompt.lerLinha("Digite o nome da pessoa " + (i + 1) + ": ");
+            String sexo;
+
+            do {
+                sexo = Prompt.lerLinha("Digite o sexo da pessoa (H para homem, M para mulher): ");
+                sexo = sexo.toUpperCase(); 
+            } while (!sexo.equals("H") && !sexo.equals("M")); 
+
+            if (sexo.equals("H")) {
+                System.out.println(nome + " - Homem");
+                totalHomens++;
+            } else if (sexo.equals("M")) {
+                System.out.println(nome + " - Mulher");
+                totalMulheres++;
+            }
+        }
+    }
+}
