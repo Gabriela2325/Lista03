3f73a08bfaa2235abf4b861ffa3e781d2cf00667
Gabriela Rodrigues Schorr <gabibusuda123@gmail.com>
2024-03-19 21:54:30 -0300

    Class17

--- Exercicio17.java
+++ Exercicio17.java
@@ -7,32 +7,24 @@ public class Exercicio17 {
         // Calcule o seu novo salárioreajustado. Escrever o nome do funcionário, o reajuste e seu novo salário.
         // Calcule quanto àempresa vai aumentar sua folha de pagamento.
 
-    String nomeFuncionario = Prompt.lerLinha("Digite o nome do funcionário: ");
+        String nomeFuncionario = Prompt.lerLinha("Digite o nome do funcionário: ");
 
-    double salariofuncionario = Prompt.lerDecimal("Digite o salário do funcionário: ");
-
-    double minimo = Prompt.lerDecimal("Digite o valor do salário mínimo: ");
-
-    double novo = Calcularsalarionovo(salariofuncionario, minimo);
-
-    System.out.println("Nome do funcionário: " + nomeFuncionario);
-    System.out.println("Reajuste: " + (novo - salariofuncionario));
-    System.out.println("Novo salário: " + novo);
-
-    double aumentoFolha = novo - salariofuncionario;
-    System.out.println("Aumento na folha de pagamento da empresa: " + aumentoFolha);
-    }
-
-    public static double Calcularsalarionovo(double salario, double salarioMinimo) {
-        if (salario < 3 * salarioMinimo) {
-            return salario * 1.5; 
-        } else if (salario >= 3 * salarioMinimo && salario <= 10 * salarioMinimo) {
-            return salario * 1.2; 
-        } else if (salario > 10 * salarioMinimo && salario <= 20 * salarioMinimo) {
-            return salario * 1.15; 
-        } else {
-            return salario * 1.1; 
+        double salarioFuncionario = Prompt.lerDecimal("Digite o salário do funcionário: ");
+        while (salarioFuncionario < 0) {
+            System.out.println("Salário não pode ser negativo.");
+            salarioFuncionario = Prompt.lerDecimal("Digite o salário do funcionário: ");
         }
 
-    }        
+        double salarioMinimo = Prompt.lerDecimal("Digite o valor do salário mínimo: ");
+        while (salarioMinimo < 0) {
+            System.out.println("Valor do salário mínimo não pode ser negativo.");
+            salarioMinimo = Prompt.lerDecimal("Digite o valor do salário mínimo: ");
+        }
+
+        Funcionario17 funcionario = new Funcionario17(nomeFuncionario, salarioFuncionario, salarioMinimo);
+        funcionario.calcularSalarioNovo();
+
+        System.out.println(funcionario.toString());
+
+    }
 }
