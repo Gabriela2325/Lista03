ff8d986858dd9f0b00a8de5962a2ab82603a26c5
Gabriela Rodrigues Schorr <gabibusuda123@gmail.com>
2024-03-19 22:14:47 -0300

    Classe21

--- Exercicio21.java
+++ Exercicio21.java
@@ -7,27 +7,12 @@ public class Exercicio21 {
     //Infantil A = 5 - 7 anosInfantil B = 8 - 10 anosjuvenil A = 11- 13 anosjuvenil B = 14 - 17 
     //anosSênior = 18 - 25 anosApresentar mensagem “idade fora da faixa etária” quando for outro ano não contemplado
 
-    int idade = Prompt.lerInteiro("Digite a idade do nadador: ");
+    int idadeNadador = Prompt.lerInteiro("Digite a idade do nadador: ");
 
-    String categoria = Classificacao(idade);
+    Narrador21 narrador = new Narrador21(idadeNadador);
 
-    System.out.println("Categoria do nadador: " + categoria);
-    }
-
-    public static String Classificacao(int idade) {
-
-        if (idade >= 5 && idade <= 7) {
-            return "Infantil A";
-        } else if (idade >= 8 && idade <= 10) {
-            return "Infantil B";
-        } else if (idade >= 11 && idade <= 13) {
-            return "Juvenil A";
-        } else if (idade >= 14 && idade <= 17) {
-            return "Juvenil B";
-        } else if (idade >= 18 && idade <= 25) {
-            return "Sênior";
-        } else {
-            return "Fora da faixa etária";
-        }
+    String categoriaNadador = narrador.classificarNadador();
+    System.out.println("Categoria do nadador: " + categoriaNadador);
+    
     }
 }        
