9e32e4e723b1898cdd06653ab8459c0b03251aff
Gabriela Rodrigues Schorr <gabibusuda123@gmail.com>
2024-03-19 21:14:53 -0300

    Class14

--- Exercicio14.java
+++ Exercicio14.java
@@ -1,47 +1,28 @@
+import java.util.List;
+
 public class Exercicio14 {
     public static void executar(){
 
-        System.out.println("Exercicio14");
+        System.out.println("Exercicio13");
 
-        //14. Faça um programa que receba o preço de custo e o preço de venda de 40 produtos.
-        // Mostrecomo resultado se houve lucro, prejuízo ou empate para cada produto. 
-        //Informe média de preço decusto e do preço de venda.
+    //14. Faça um programa que receba o preço de custo e o preço de venda de 40 produtos. 
+    //Mostrecomo resultado se houve lucro,
+    // prejuízo ou empate para cada produto. Informe média de preço decusto e do preço de venda
 
-        double Custo = 0;
-        double Venda = 0;
-        int Produtos = 0;
-        int Lucro = 0;
-        int Prejuizo = 0;
-        int Empate = 0;
+        List<Produto14> produtos = Produto14.criarProdutos();
 
-        for (int i = 0; i < 40; i++) {
-            double precoCusto = Prompt.lerDecimal("Digite o preço de custo do produto " + (i + 1) + ": ");
-            double precoVenda = Prompt.lerDecimal("Digite o preço de venda do produto " + (i + 1) + ": ");
-
-            Custo += precoCusto;
-            Venda += precoVenda;
-            Produtos++;
-
-            if (precoVenda > precoCusto) {
-                System.out.println("Produto " + (i + 1) + ": Lucro");
-                Lucro++;
-            } else if (precoVenda < precoCusto) {
-                System.out.println("Produto " + (i + 1) + ": Prejuízo");
-                Prejuizo++;
-            } else {
-                System.out.println("Produto " + (i + 1) + ": Empate");
-                Empate++;
-            }
+        for (Produto14 produto : produtos) {
+            double precoCusto = Math.random() * 100; 
+            double precoVenda = precoCusto + (Math.random() * 50); 
+            produto.setPrecoCusto(precoCusto);
+            produto.setPrecoVenda(precoVenda);
         }
 
-        double mediaPrecoCusto = Custo / Produtos;
-        double mediaPrecoVenda = Venda / Produtos;
-
-        System.out.println("\nMédia de preço de custo: " + mediaPrecoCusto);
-        System.out.println("Média de preço de venda: " + mediaPrecoVenda);
-        System.out.println("Total de produtos com lucro: " + Lucro);
-        System.out.println("Total de produtos com prejuízo: " + Prejuizo);
-        System.out.println("Total de produtos com empate: " + Empate);
+        for (int i = 0; i < produtos.size(); i++) {
+            System.out.println("Produto " + (i+1) + ":");
+            System.out.println(produtos.get(i));
+            System.out.println("-----------------------------");
+        }
     }
 }
 
